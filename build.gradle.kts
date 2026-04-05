583c2535b7718bfc3927549fa500feab5375ebbf
Avi Levin <aviavi23@gmail.com>
2026-04-05 16:04:10 +0300

    build(deps): upgrade AGP to 9.1.0, Gradle to 9.3.1, and Kotlin to 2.2.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,16 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.ktlint) apply false
     alias(libs.plugins.detekt) apply false
     alias(libs.plugins.dokka)
     alias(libs.plugins.vanniktech.publish) apply false
+    alias(libs.plugins.skie) apply false
 }
 
 allprojects {
